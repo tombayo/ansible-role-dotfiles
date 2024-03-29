e0ff084a6d7f61b481e901be46bfc18eaee43aeb
Tom A Munkhaug <git@tombayo.com>
2024-03-29 12:53:54 +0100

    Added the dots

---
+++ files/.config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases:
+alias s="ssh"
+alias c="clear"
+alias ls="eza"
+alias cat="batcat"
+
+# Settings:
+
